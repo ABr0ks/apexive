1ca772be9d393ed7dc1d19570df91e8172a8048a
AB <abhilash2923@gmail.com>
2024-04-11 16:14:25 +0530

    [IMP] invoice_payment_reminder: added the documentation

--- invoice_payment_reminder/__manifest__.py
+++ invoice_payment_reminder/__manifest__.py
@@ -2,13 +2,13 @@
     'name': 'Invoice Due Payment Reminder',
     'version': '17.0.1.0',
     'summary': 'Invoice due payment reminders for timely follow-up.',
-    'description': 'The Payment Reminder module automates the process of sending payment reminders to customers or clients with outstanding invoices or pending payments.' 
+    'description': 'The Payment Reminder module automates the process of sending payment reminders to customers with pending payments.' 
                     'This helps businesses maintain healthy cash flow and minimize late payments.' 
                     'With customizable email templates and scheduling options, users can efficiently manage reminders,'
                     ' reducing manual effort and ensuring timely follow-up with customers.',
     'author': 'Abhilash Pathak',
     'maintainer': 'Abhilash Pathak',
-    'depends': ['account'],
+    'depends': ['account', 'mail'],
     'data': [
         'data/payment_reminder_cron.xml',
         'data/payment_reminder_email_template.xml',
